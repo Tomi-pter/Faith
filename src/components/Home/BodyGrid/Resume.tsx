32173e4f6f8810061fa26ad7b652258df3e4b401
Tomi <tomiaiyeniko@penzack.com>
2025-07-18 17:27:06 +0100

    open link in new tab

--- src/components/Home/BodyGrid/Resume.tsx
+++ src/components/Home/BodyGrid/Resume.tsx
@@ -4,6 +4,7 @@ const Resume = () => {
   return (
     <a
       href="https://drive.google.com/file/d/1YWzeqT5aSAxY9JP596pLp3TT10v0qdR_/view?usp=drivesdk"
+      target="_blank"
       className="bg-[#F5C9E0] col-span-3 p-9 text-left flex flex-col gap-4 bg-[url(/src/assets/resume_bg.png)] bg-no-repeat bg-center bg-size-[50%] md:bg-auto min-h-[350px] justify-between mx-8 md:m-0"
     >
       <div className="flex gap-4 items-center">
